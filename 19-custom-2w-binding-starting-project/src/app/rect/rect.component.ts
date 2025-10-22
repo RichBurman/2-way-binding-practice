e1b7077a82c20755ffe2063be96b179efbc815c9
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-22 12:31:49 +0100

    Implement custom two-way binding for rectangle size in rect component

--- 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -11,8 +11,12 @@ export class RectComponent {
   // Todo: Implement custom two-way binding
 
   size = input.required<{width: string; height: string}>()
+  sizeChange = output<{width: string; height: string}>()
 
   onReset() {
-    // ...
+    this.sizeChange.emit({
+      width: '200',
+      height: '100'
+    })
   }
 }
